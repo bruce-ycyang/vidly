e61b7a7cc35b4bacfb57dbf977659e9077c07728
bruce <bruce@aibdt.com.tw>
2022-06-21 17:37:02 +0800

    finish ch5

---
+++ src/components/common/table.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import TableBody from './tableBody';
+import TableHeader from './tableHeader';
+
+const Table = ({ sortColumn ,onSort, columns, data }) => {
+
+	return (
+		<table className="table">
+			<TableHeader columns={columns} sortColumn={sortColumn} onSort={onSort}></TableHeader>
+			<TableBody data={data} columns={columns}/>
+		</table>  
+	);
+}
+ 
+export default Table;
---
+++ src/components/moviesTable.jsx
@@ -0,0 +1,28 @@
+import React, {Component} from 'react';
+import Like from './common/like';
+import Table from './common/table';
+
+class MoviesTable extends Component {
+	columns = [
+		{path: 'title', label:'Title'},
+		{path: 'genre.name', label:'Genre'},
+		{path: 'numberInStock', label:'Stock'},
+		{path: 'dailyRentalRate', label:'Rate'},
+		{key: 'Like',content: movie => <Like liked={movie.liked} onClick={() =>this.props.onLike(movie)}/>},
+		{key: 'Delete', content: movie => <button onClick={() => this.props.onDelete(movie)} className="btn btn-danger btn-sm">Delete</button>},
+	]
+	
+	render() { 
+		const { sortColumn ,onSort, movies } = this.props;
+
+		return ( 
+
+			<Table columns={this.columns} data={movies} sortColumn={sortColumn} onSort={onSort}/>
+		);
+	}
+
+}
+ 
+	
+ 
+export default MoviesTable;
